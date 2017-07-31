6f5cad402a8297b599b41dd5fd00ef1a363cd62c
Ary Davani <arydavani@gmail.com>
2017-07-31 18:22:21 -0400

    styling lols

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # React Router Blog
 - Create a Blog application that uses React and React Router, and generates dynamic endpoints.
+<p className="logo">PolyBlog</p>
--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -5,7 +5,16 @@ export default function BaseLayout(props) {
   return (
     <div>
       <div className="flex nav-bar">
-        <NavLink to="/"><p className="logo">PolyBlog</p></NavLink>
+        <NavLink to="/">
+          <span className="logo1">P</span>
+          <span className="logo2">o</span>
+          <span className="logo3">l</span>
+          <span className="logo4">y</span>
+          <span className="logo5">B</span>
+          <span className="logo6">l</span>
+          <span className="logo7">o</span>
+          <span className="logo8">g</span>
+        </NavLink>
         <NavLink to="/create-post">Create Post</NavLink>
         <NavLink to="/show-posts">Show All Posts</NavLink>
       </div>
